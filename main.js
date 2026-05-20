f88f68028735bb80043e0f4584b1c48717ed5c09
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-20 17:35:50 +0200

    ADD prove con process.argv

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ console.log('Hello World');
 console.log('Hello Boolean');
 
 // const fraseDaTerminale = process.argv
+
+// Prendiamo solo il nostro argomento:
 const fraseDaTerminale = process.argv.slice(2)
 
 console.log(fraseDaTerminale);
