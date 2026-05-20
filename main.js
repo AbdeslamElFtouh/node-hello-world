30a2d4d2e7a5efbee2b2a816e20f80583a464ff2
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-20 18:19:05 +0200

    ADD cowsayjs prova per stampare stringhe con cowsay

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import {cowsay} from 'cowsayjs';
+
 console.log('Hello World');
 console.log('Hello Boolean');
 
@@ -5,5 +7,9 @@ console.log('Hello Boolean');
 
 // Prendiamo solo il nostro argomento:
 const fraseDaTerminale = process.argv.slice(2)
-
 console.log(fraseDaTerminale);
+
+const arrayDiStringhe = ['Questa ',' è ',' Spartaaaa']
+
+console.log('Questa è follia...');
+console.log(cowsay(`${arrayDiStringhe}`));
