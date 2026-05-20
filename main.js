96fac8f0ccc1bea281878f55a8d427345599a947
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-20 17:33:55 +0200

    ADD script watch per vedere l'aggiornamento del file in tempo reale

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log(('Hello World'));
+console.log('Hello World');
+console.log('Hello Boolean');
+
+// const fraseDaTerminale = process.argv
+const fraseDaTerminale = process.argv.slice(2)
+
+console.log(fraseDaTerminale);
